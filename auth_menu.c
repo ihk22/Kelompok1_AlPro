3457a60ce475c5a2e5360e30f7d83f8295829a87
puyukentangwle <putriayudahlianainggolan@student.upi.edu>
2025-12-20 17:44:09 +0700

    feat: auth & menu admin-mahasiswa (Putri Ayu Dahlia Nainggolan)

---
+++ auth_menu.c
@@ -0,0 +1,64 @@
+// --- CEK LOGIN ---
+int cekLogin(char *user, char *pass, char *roleWajib) {
+    FILE *fp = fopen(FILE_LOGIN, "r");
+    char buffer[100];
+    char fileUser[20], filePass[20], fileRole[10];
+    int loginSukses = 0;
+
+    if (fp == NULL) {
+        printf("[!] ERROR: File '%s' tidak ditemukan!\n", FILE_LOGIN);
+        return 0;
+    }
+
+    while (fgets(buffer, sizeof(buffer), fp)) {
+        sscanf(buffer, "%[^|]|%[^|]|%s", fileUser, filePass, fileRole);
+        if (strcmp(user, fileUser) == 0 &&
+            strcmp(pass, filePass) == 0 &&
+            strcmp(roleWajib, fileRole) == 0) {
+            loginSukses = 1;
+            break;
+        }
+    }
+    fclose(fp);
+    return loginSukses;
+}
+
+// --- MENU ADMIN ---
+void menuAdmin() {
+    char id[20], pass[20];
+    printf("\n--- LOGIN ADMIN ---\n");
+    printf("ID Admin : "); scanf("%s", id);
+    printf("Password : "); scanf("%s", pass);
+    bersihkanBuffer();
+
+    if (cekLogin(id, pass, "ADMIN")) {
+        int pilihan;
+        do {
+            printf("\n1. Input/Update Nilai\n");
+            printf("2. Kembali\n");
+            printf("Pilihan: ");
+            scanf("%d", &pilihan);
+            bersihkanBuffer();
+            if (pilihan == 1) inputDataNilai();
+        } while (pilihan != 2);
+    } else {
+        printf("[!] Login gagal\n");
+        getchar();
+    }
+}
+
+// --- MENU MAHASISWA ---
+void menuMahasiswa() {
+    char nim[20], pass[20];
+    printf("\n--- LOGIN MAHASISWA ---\n");
+    printf("NIM : "); scanf("%s", nim);
+    printf("Password : "); scanf("%s", pass);
+    bersihkanBuffer();
+
+    if (cekLogin(nim, pass, "MHS")) {
+        lihatNilaiSesuaiNIM(nim);
+    } else {
+        printf("[!] Login gagal\n");
+        getchar();
+    }
+}
